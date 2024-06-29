c24c65e9c50fe40f5e450ce23d685c72e894e47d
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-06-29 09:45:00 +0200

    added in components and vjs code

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,25 @@
+<template>
+      <h3>History</h3>
+      <ul id="list" class="list">
+        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+          {{ transaction.text }} <span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
+        </li>
+      </ul>
+</template>
+
+//Options API
+
+<script>
+  export default {
+    data() {
+      return {
+        transactions: [
+          { id: 1, text: 'Flowers', amount: -19.99 },
+          { id: 2, text: 'Salary', amount: 300 },
+          { id: 3, text: 'Book', amount: -10 },
+          { id: 4, text: 'Camera', amount: 150 }
+        ]
+      }
+    }
+  }
+</script>
